5eb030e6e118cd9da226578f2aeb0e4d43d177b5
Hayri ACAR <hacar@intiformation.fr>
2024-06-12 12:18:08 +0200

    semanticui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Icon, Input, Pagination, Container } from 'semantic-ui-react'
+import React, { Fragment } from 'react';
+import Menu from './components/Menu';
+import Login from './components/Login';
 
 function App() {
+  function call() {
+    alert("Hello World !");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b><p> Semantic UI React Integration </p></b>
+      <hr /> <br /><strong> Alert button: </strong>
+      <br /> < br />
+      <Button onClick={call} className="icon"
+        labelPosition='right' >
+        Notificatons
+        < Icon name='right bell' />
+      </Button>
+
+      <div className="ui container"
+        style={{ textAlign: "center" }}>
+        <br /><br />
+        <b><p>Semantic UI Input Labeled Variation in React</p></b>
+        <hr /><br />
+        <Input
+          label={{ basic: true, content: '€' }}
+          labelPosition='right'
+          placeholder='Saisir le montant'
+        /><br /><br />
+        <Input action='Login' placeholder='Saisir mail' />
+      </div>
+
+      <Pagination defaultActivePage={2} totalPages={8} />
+      <Pagination defaultActivePage={2} totalPages={8} disabled />
+
+      <Fragment>
+        <Menu />
+        <Container>
+          <Login />
+        </Container>
+      </Fragment>
+
     </div>
   );
 }
